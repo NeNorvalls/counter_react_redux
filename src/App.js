b70f255cb4167850e22b4bdb2c8253ca5ed2e06f
NeNorvalls <honeygeeh27@gmail.com>
2023-11-21 02:32:22 +0100

    fixing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement } from "./actions/counterAction";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import App from "./App";
+import store from "./store";
+import "./App.css";
 
-const App = () => {
-  const count = useSelector((state) => state.counter.count);
-  const dispatch = useDispatch();
+const root = document.getElementById("root");
 
-  return (
-    <div>
-      <h1>Counter App</h1>
-      <p>Count: {count}</p>
-      <button onClick={() => dispatch(increment())}>Increment</button>
-      <button onClick={() => dispatch(decrement())}>Decrement</button>
-    </div>
-  );
-};
-
-export default App;
+const reactRoot = ReactDOM.createRoot(root);
+reactRoot.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
